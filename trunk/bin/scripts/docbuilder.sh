a21c8a11027325de7c000425fd1bef9b3f94b4dc
sekati <sekati@b1c2fec6-6530-0410-9bf4-f9e2c5e9b997>
2007-05-14 06:21:07 +0000

    Initial import.

---
+++ trunk/bin/scripts/docbuilder.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+####################################################################################
+# AS2 Documentation Builder - v.0.0.1 - jason m horwitz | sekati.com
+# Copyright (C) 2007  jason m horwitz, Sekat LLC. All Rights Reserved.
+# Released under the MIT License: http://www.opensource.org/licenses/mit-license.php
+#
+# Usage: Copy script to your project/build directory and edit config sections below.
+#
+####################################################################################
+# CONFIGURATION - edit these to suite your environment
+
+PROJECT_TITLE="SASAPI - Sekati ActionScript API"
+PROJECT_PATH="/Users/jason/Desktop/workspace/sasapi"
+DOC_PATH="$PROJECT_PATH/doc"
+CLASS_PATH="$PROJECT_PATH/src"
+PACKAGES="com.sekati.*"
+
+FDTK_PATH="/Users/jason/Desktop/workspace/FDTKit"
+AS2API_PATH="$FDTK_PATH/bin/as2api"
+DOT="$FDTK_PATH/bin/graphviz/Graphviz.app/Contents/MacOS/dot"
+
+####################################################################################
+
+echo "DOCBUILDER: Generating $TITLE docs from [$CLASS_PATH] to [$DOC_PATH] ..."
+
+# clear outdated docs
+rm -rf $DOC_PATH/*
+
+cd $AS2API_PATH
+ruby as2api.rb package-spec $PACKAGES --classpath $CLASS_PATH --output-dir $DOC_PATH --title $PROJECT_TITLE --sources --progress --draw-diagram --dot-exe $DOT
+
+cp "$DOC_PATH/frameset.html" "$DOC_PATH/index.html"
+#cd ..
+
+####################################################################################
+
+echo "DOCBUILDER: complete."
+exit 0
+
+####################################################################################
---
+++ trunk/bin/scripts/syncsite.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+####################################################################################
+# SyncSite - v.0.0.3 - ssh/rsync dev to production - jason m horwitz | sekati.com
+# Copyright (C) 2007  jason m horwitz, Sekat LLC. All Rights Reserved.
+# Released under the MIT License: http://www.opensource.org/licenses/mit-license.php
+#
+# Usage: Copy script to your project/build directory and edit config sections below.
+#
+####################################################################################
+# CONFIGURATION - edit these to suite your environment
+
+SOURCE="/Users/jason/Desktop/workspace/sasapi/"
+DEST_HOST="sekati.com"
+DEST_PATH="/www/dev/sasapi/"
+DEST_USER="root"
+EXCLUDE="*bak*,*BAK*,*.tmp,*.TMP,.DS_Store,Thumbs.db,.exclude_buildsite"
+
+####################################################################################
+
+echo "SYNCSITE: building exclude list ..."
+echo $EXCLUDE | sed -e 's/\,/\
+/g' >> .exclude_buildsite
+
+echo "SYNCSITE: syncing ($SOURCE)-->($DEST_HOST:$DEST_PATH)\n"
+rsync -ave ssh --delete --force --exclude-from=".exclude_buildsite" $SOURCE $DEST_HOST:$DEST_PATH
+
+echo "\nSYNCSITE: CLEANING UP ..."
+rm .exclude_buildsite
+
+echo "SYNCSITE: complete."
+exit 0
+
+####################################################################################
